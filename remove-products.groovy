fe1cf85e62087de0d7ef15882bb07933266d3534
EL AISSOUSSI Monsif <monsif20@gmail.com>
2018-04-16 09:31:02 +0200

    Create remove-products.groovy

---
+++ remove-products.groovy
@@ -0,0 +1,44 @@
+def query = "****";
+	
+def fsq = new FlexibleSearchQuery(query)
+fsq.setCount(200)
+
+// iteration number to determine using select count(pk)
+for (int i = 0 ; i < 1 ; i++ )
+{
+
+println "start iteration " + i
+
+def timeSlots = flexibleSearchService.search(fsq).getResult()
+
+
+Transaction tx = Transaction.current()
+tx.begin()
+
+boolean success = false;
+println "Time slot size " + timeSlots.size()
+try 
+{
+
+	modelService.removeAll(timeSlots);
+	success = true;
+}
+catch (final Exception exception) 
+{
+
+	println "Exception : {} ",exception.getMessage();
+}
+finally
+{
+	if(success) 
+        {
+		tx.commit();
+	}
+	else 
+        {
+		tx.rollback();
+	}
+}
+
+println "End iteration " + i+" success = "+success
+}
